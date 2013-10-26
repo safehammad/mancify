b7f68611a3d2ddda5aa8007d4c7ef77b92a75758
Safe Hammad <safe.hammad@sandacre.com>
2013-10-26 18:00:49 +0100

    Translator now returns text rather than list of tokens.

--- test.py
+++ test.py
@@ -1,16 +1,30 @@
 import unittest
+from itertools import chain
 
 import translator
 import manc
 
 
 class TestMancify(unittest.TestCase):
-    def testTranslate(self):
-        sample = 'A line. &A line ^with symbols.\nThe next line.\nAnother line.'
-        expected = ['A', 'lain', '.', '&', 'A', 'lain', '^', 'wihth', 'sihmbulz', '.', '\n', 'The', 'nehkst', 'lain', '.', '\n', 'Another', 'lain', '.']
-        actual = translator.translate(sample,manc)
+    def testTokenize(self):
+        text = 'A line. &A line ^with symbols.\nThe next line.\nAnother line.'
+        expected = ['A', 'line', '.', '&', 'A', 'line', '^', 'with', 'symbols', '.', '\n', 'The', 'next', 'line', '.', '\n', 'Another', 'line', '.']
+        actual = translator.tokenize(text)
         self.assertItemsEqual(expected, actual)
 
-    def testReplace(self):
-        self.assertTrue(translator.replace_random('bad',manc) in ("naff","shit","knackered","buggered","pants","pear-shaped","minging",))
-        self.assertTrue(translator.replace_random('poor',manc) in ("naff","shit","knackered","buggered","pants","pear-shaped","minging",))
+    def testTranslateWord(self):
+        replacements = ['naff', 'shit', 'knackered', 'buggered', 'pants', 'pear-shaped', 'minging']
+        self.assertIn(translator.translate('bad'), replacements)
+        self.assertIn(translator.translate('poor'), replacements)
+
+    def translate(self):
+        text = 'A line. &A line ^with symbols.\nThe next line.\nAnother line.'
+        expected = ['A', 'lain', '.', '&', 'A', 'lain', '^', 'wihth', 'sihmbulz', '.', '\n', 'The', 'nehkst', 'lain', '.', '\n', 'Another', 'lain', '.']
+        actual = translator.translate(text)
+        self.assertItemsEqual(expected, actual)
+
+    def testSpacer(self):
+        # Check whitespace is maintine
+        text = 'This is a very, very good test!\nLine two. And again!'
+        tokens = translator.tokenize(text)
+        self.assertEqual(text, translator.untokenize(chain(tokens)))
